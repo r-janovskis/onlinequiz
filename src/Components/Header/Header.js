64dd9a91a858be56a5ea0928a5cc664da2425e33
Reins Janovskis <reinis.janovskis@hotmail.com>
2024-07-16 21:24:12 +0100

    Added rotating question mark in header. Adjusted some styling

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./Header.css";
+import q_mark from "../../Images/big_q.png";
 
 function Header(props) {
   return (
     <header>
-      <h1>The Amazing Online Quiz!</h1>
-      <p>Some sample text in Poppin</p>
+      <h1>The Amazing Online Quiz</h1>
+      <img src={q_mark} alt="Rotating logo" />
     </header>
   );
 }
