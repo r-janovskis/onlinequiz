14b0f72885f467f59abc296785b4f2d8ff4f7d44
Reins Janovskis <reinis.janovskis@hotmail.com>
2024-07-23 13:44:49 +0100

    Changed font for titles -> h1

---
+++ src/Components/GameType/GameType.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./GameType.css";
+
+function GameType(props) {
+  return (
+    <div className="container">
+      <h1>Choose Game</h1>
+    </div>
+  );
+}
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -5,7 +5,7 @@ import q_mark from "../../Images/big_q.png";
 function Header(props) {
   return (
     <header>
-      <h1>The Amazing Online Quiz</h1>
+      <h1>The Amazing Online Quiz!</h1>
       <img src={q_mark} alt="Rotating logo" />
     </header>
   );
