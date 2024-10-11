3175eab559ad0e3aa532b3a93a27110f32bdaae8
Reins Janovskis <reinis.janovskis@hotmail.com>
2024-10-11 13:09:28 +0100

    Clean up Quiz.js by removing commented out chunks of old code

--- src/Components/Quiz/Quiz.js
+++ src/Components/Quiz/Quiz.js
@@ -9,41 +9,27 @@ import {
   quizTopic,
   questionCount,
 } from "../Quiz/QuizSlice";
-import QuizCard from "../QuizCard/QuizCard";
 import "./Quiz.css";
 
 import { useParams, useNavigate, Outlet, Link } from "react-router-dom";
 
 function Quiz(props) {
-  //const { quizTopic } = useParams();
-
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const quizQuestions = useSelector(questions);
   const questionNumber = useSelector(index);
   const quizTitle = useSelector(quizTopic);
   const numberOfQuestions = useSelector(questionCount);
-  //quizTitle = quizTitle.charAt(0).toUpperCase() + quizTitle.slice(1);
 
   let correctAnswer;
-
-  //console.log(quizQuestions);
-
-  //const question = props.question;
 
   function handleSelectAnswer(event) {
     const buttonClicked = event.target;
     if (buttonClicked.textContent === correctAnswer) {
       buttonClicked.classList.add("correct");
-      // document
-      //   .getElementsByClassName("answerCube")
-      //   [props.index].classList.add("correctCube");
       dispatch(answerCorrectly());
     } else {
       buttonClicked.classList.add("incorrect");
-      // document
-      //   .getElementsByClassName("answerCube")
-      //   [props.index].classList.add("incorrectCube");
     }
 
     const answerButtons = document.getElementsByClassName("answerButton");
@@ -58,7 +44,6 @@ function Quiz(props) {
     }
 
     document.getElementById("nextButton").style.display = "block";
-    //props.onAnswerQuestion();
   }
 
   const [correctAnswers, setCorrectAnswers] = useState(0);
@@ -74,7 +59,6 @@ function Quiz(props) {
       answerButtons[i].classList.remove("correct");
       answerButtons[i].classList.remove("incorrect");
       answerButtons[i].classList.remove("correctAfterAnswer");
-      // answerButtons[i].disabled = false;
       answerButtons[i].classList.remove("deactivated");
     }
 
@@ -82,12 +66,8 @@ function Quiz(props) {
       navigate(`/${quizTitle}/results`);
     }
 
-    // console.log(`Before: ${questionNumber}, ${numberOfQuestions}`);
     dispatch(nextQuestion());
-    // const newIndex = useSelector(index);
-    // console.log(`After dispatch: ${newIndex}, ${numberOfQuestions}`);
     document.getElementById("nextButton").style.display = "none";
-    // navigate(`/${quizTitle}/${newIndex + 1}`);
   }
 
   useEffect(() => {
@@ -95,46 +75,7 @@ function Quiz(props) {
       navigate(`/${quizTitle}/${questionNumber + 1}`);
     }
   }, [questionNumber]);
-  /*
-  if (props.index + 1 === props.numberOfQuestions) {
-    return (
-      <div className="container">
-        <h1>{props.quizTopic} Quiz</h1>
-        <QuizCard
-          question={props.question}
-          onAnswerQuestion={props.onAnswerQuestion}
-          onCorrectAnswer={props.onCorrectAnswer}
-          index={props.index}
-        />
-        <button
-          id="nextButton"
-          className="btn btn-lg mainButton"
-          onClick={props.onEndGame}
-        >
-          Finish Quiz
-        </button>
-      </div>
-    );
-  }
-  return (
-    <div className="container">
-      <h1>{props.quizTopic} Quiz</h1>
-      <QuizCard
-        question={props.question}
-        onAnswerQuestion={props.onAnswerQuestion}
-        onCorrectAnswer={props.onCorrectAnswer}
-        index={props.index}
-      />
-      <button
-        id="nextButton"
-        className="btn btn-lg mainButton"
-        onClick={props.onNextQuestion}
-      >
-        Next
-      </button>
-    </div>
-  );
-  */
+
   if (quizQuestions.length === 0 || !quizQuestions) {
     return (
       <article>
