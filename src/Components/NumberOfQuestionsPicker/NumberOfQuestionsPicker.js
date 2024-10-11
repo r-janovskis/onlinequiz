92ed9b78bf381893131c0d826ff36f095c33e49c
Reins Janovskis <reinis.janovskis@hotmail.com>
2024-10-11 15:49:55 +0100

    Create component to select number of questions in a quiz

---
+++ src/Components/NumberOfQuestionsPicker/NumberOfQuestionsPicker.js
@@ -0,0 +1,35 @@
+import React, { useEffect } from "react";
+import "./NumberOfQuestionsPicker.css";
+
+function NumberOfQuestionsPicker() {
+  const handleChange = (event) => {
+    document.getElementById("displayNumberOfQuestions").innerHTML =
+      event.target.value;
+    console.log(event.target.value);
+  };
+
+  useEffect(() => {
+    document.getElementById("numberOfQuestions").value = 10;
+    document.getElementById("displayNumberOfQuestions").innerHTML = 10;
+  }, []);
+
+  return (
+    <section className="numberPicker">
+      <div>
+        <label className="form-label">Number of Questions in Quiz</label>
+        <input
+          type="range"
+          className="form-range"
+          min="5"
+          max="20"
+          step="1"
+          id="numberOfQuestions"
+          onChange={handleChange}
+        />
+      </div>
+      <p id="displayNumberOfQuestions"></p>
+    </section>
+  );
+}
+
+export default NumberOfQuestionsPicker;
